fbbb6fbeeebb2ebbef53ba79e8b2483bc9afb439
Stephen Prior <stephen@prior-technology.ie>
2021-08-02 00:44:37 +0100

    Cookie Cut

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyses and annotates the internal representations of natural language text processed through a generative transformer model.',
+    author='Stephen Prior',
+    license='MIT',
+)
